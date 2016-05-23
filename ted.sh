13dbf9c4b019f169f9a785e6fbedddaf7afabd6c
ezzobad <md_salehi@yahoo.com>
2016-05-23 15:50:25 +1000

    public IP fixed

--- ted.sh
+++ ted.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-export instanceId=`curl  http://169.254.169.254/latest/meta-data/instance-id`
-export AZ=`curl  http://169.254.169.254/latest/meta-data/placement/availability-zone/`
-export PrivateIP=`curl  http://169.254.169.254/latest/meta-data/local-ipv4/`
-export PublicIP=`curl  http://169.254.169.254/latest/meta-data/public-ipv4/`
-
+# Populated the index.html file with proper instance information.
+# By: Mehdi (awsengineer.com)
 wget https://raw.githubusercontent.com/ezzobad/ted/master/ted.html -O /var/www/html/index.html
 wget https://raw.githubusercontent.com/ezzobad/ted/master/homerwoohoo.jpg  -O /var/www/html/homerwoohoo.jpg
+
+export instanceId=`curl  http://169.254.169.254/latest/meta-data/instance-id`
 sed -i "s/blah/$instanceId/" /var/www/html/index.html
-if [ "$PublicIP" =  "" ]
+
+export AZ=`curl  http://169.254.169.254/latest/meta-data/placement/availability-zone/`
+sed -i "s/us-east-1/$AZ/" /var/www/html/index.html
+
+export PrivateIP=`curl  http://169.254.169.254/latest/meta-data/local-ipv4/`
+sed -i "s/1.1.1.1/$PrivateIP/" /var/www/html/index.html
+
+curl http://169.254.169.254/latest/meta-data/public-ipv4/   | grep 404
+if [ $? -eq 0 ]
 then
         sed -i "s/8.8.8.8/N\/A/" /var/www/html/index.html
 else
+        export PublicIP=`curl  http://169.254.169.254/latest/meta-data/public-ipv4/`
         sed -i "s/8.8.8.8/$PublicIP/" /var/www/html/index.html
 fi
-sed -i "s/1.1.1.1/$PrivateIP/" /var/www/html/index.html
-sed -i "s/us-east-1/$AZ/" /var/www/html/index.html
