bd8fad78267a83f03db494c707319477287574a9
ezzobad <md_salehi@yahoo.com>
2016-05-23 14:19:56 +1000

    updated with the new homer photo and pointing to a new html

--- ted.sh
+++ ted.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# By: Mehdi
 export instanceId=`curl  http://169.254.169.254/latest/meta-data/instance-id`
 export AZ=`curl  http://169.254.169.254/latest/meta-data/placement/availability-zone/`
 export PrivateIP=`curl  http://169.254.169.254/latest/meta-data/local-ipv4/`
 export PublicIP=`curl  http://169.254.169.254/latest/meta-data/public-ipv4/`
 
 wget https://raw.githubusercontent.com/ezzobad/ted/master/ted.html -O /var/www/html/index.html
-sed -i "s/blah1/$instanceId/" /var/www/html/index.html
+wget https://raw.githubusercontent.com/ezzobad/ted/master/homerwoohoo.jpg  -O /var/www/html/homerwoohoo.jpg
+sed -i "s/blah/$instanceId/" /var/www/html/index.html
 sed -i "s/8.8.8.8/$PublicIP/" /var/www/html/index.html
 sed -i "s/1.1.1.1/$PrivateIP/" /var/www/html/index.html
 sed -i "s/us-east-1a/$AZ/" /var/www/html/index.html
