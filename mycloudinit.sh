bb0e5765b0ce00de50738ce899fd1aa6ff9b65da
ezzobad <md_salehi@yahoo.com>
2016-05-24 17:59:52 +1000

    mycloudinit

---
+++ mycloudinit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum update -y
+yum install httpd python34 python-pip -y
+pip install --upgrade pip
+sleep 5
+sync; sync; sync
+pip install sphinx htmltag
+chkconfig httpd on
+wget https://raw.githubusercontent.com/ezzobad/ted/master/ted.py -O /var/www/cgi-bin/ted.py
+chmod +x /var/www/cgi-bin/ted.py
+service httpd start
